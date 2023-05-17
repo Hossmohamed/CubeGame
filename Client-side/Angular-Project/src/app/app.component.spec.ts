ca7177e516d49c44a9d2a12b6ec48dfd7592740f
NancyNagy <nnagy686@gmail.com>
2023-05-17 15:08:26 +0300

    Angular Project

--- Client-side/Angular-Project/src/app/app.component.spec.ts
+++ Client-side/Angular-Project/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Task3'`, () => {
+  it(`should have as title 'GP-cubegame'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Task3');
+    expect(app.title).toEqual('GP-cubegame');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Task3 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('GP-cubegame app is running!');
   });
 });
