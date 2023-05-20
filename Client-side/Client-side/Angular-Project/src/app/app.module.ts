1522fff27ffcd6a435c825cc47b1c7c3890d2c9e
MarimEzz <marimezzaldin670@gmail.com>
2023-05-20 22:58:22 +0300

    Revert "edit discover and add login"
    This reverts commit 57dfae07a66ed1a216b6995dba955363fe33d7f6.

--- Client-side/Client-side/Angular-Project/src/app/app.module.ts
+++ Client-side/Client-side/Angular-Project/src/app/app.module.ts
@@ -1,18 +1,11 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-//import { RouterModule, Routes} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { StoreComponent } from './store/store.component';
 import { DistributionComponent } from './distribution/distribution.component';
 import { DiscoverComponent } from './discover/discover.component';
 import { BrowseComponent } from './browse/browse.component';
-import { LoginComponent } from './login/login.component';
-
-/*let routes:Routes = [
-  {path:"", component:DiscoverComponent},
-  {path:"Browse", component:BrowseComponent },
-]*/
 
 @NgModule({
   declarations: [
@@ -21,11 +14,9 @@ import { LoginComponent } from './login/login.component';
     DistributionComponent,
     DiscoverComponent,
     BrowseComponent,
-    LoginComponent,
   ],
   imports: [
-    BrowserModule,
-    //RouterModule.forRoot(routes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
