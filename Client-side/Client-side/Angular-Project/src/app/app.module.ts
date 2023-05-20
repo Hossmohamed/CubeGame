57dfae07a66ed1a216b6995dba955363fe33d7f6
MarimEzz <marimezzaldin670@gmail.com>
2023-05-20 22:44:22 +0300

    edit discover and add login

--- Client-side/Client-side/Angular-Project/src/app/app.module.ts
+++ Client-side/Client-side/Angular-Project/src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { RouterModule, Routes} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { StoreComponent } from './store/store.component';
 import { DistributionComponent } from './distribution/distribution.component';
 import { DiscoverComponent } from './discover/discover.component';
 import { BrowseComponent } from './browse/browse.component';
+import { LoginComponent } from './login/login.component';
+
+/*let routes:Routes = [
+  {path:"", component:DiscoverComponent},
+  {path:"Browse", component:BrowseComponent },
+]*/
 
 @NgModule({
   declarations: [
@@ -14,9 +21,11 @@ import { BrowseComponent } from './browse/browse.component';
     DistributionComponent,
     DiscoverComponent,
     BrowseComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
