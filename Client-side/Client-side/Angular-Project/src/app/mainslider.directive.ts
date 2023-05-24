62be7772c4f32c03d36f4d6f115de5274a176cc6
MarimEzz <marimezzaldin670@gmail.com>
2023-05-24 17:14:05 +0300

    added main slider it works finallyyyy! :)

--- Client-side/Client-side/Angular-Project/src/app/mainslider.directive.ts
+++ Client-side/Client-side/Angular-Project/src/app/mainslider.directive.ts
@@ -6,48 +6,17 @@ import { Directive, HostListener, ElementRef} from '@angular/core';
 export class MainsliderDirective {
 
   constructor(private el: ElementRef) { }
+
   @HostListener('click')
   imagechange(){
     var src:any =this.el.nativeElement.src;
     var prev:any = document.getElementById("preview");
     prev.src = src;
-
     var imgslide = document.getElementsByClassName("img-slide");
-      var flag = 0;
-
-     
-
     for(let i=0; i < imgslide.length; i++)
     {
       imgslide[i].classList.remove("active-slide");
     }
     this.el.nativeElement.parentElement.classList.add("active-slide");
   }
-
-
-  // src:any = this.el.nativeElement.src;
-  //     prev:any = document.getElementById('preview')
-  //     imageSlide = document.getElementsByTagName('img')
-  //     flag = 0;
-
-  //    play =  setInterval(()=>{
-  //       if (this.flag > this.imageSlide.length - 1) {
-  //         this.flag=0
-  //        }
-  //         this.prev.src = this.imageSlide[this.flag].src ;
-  //         for (let i = 0 ; i< this.imageSlide.length ; i++ ){
-  //           this.imageSlide[i].classList.remove('active');
-  //         }
-  //         this.imageSlide[this.flag].classList.add('active')
-  //        this.flag = this.flag + 1
-  //      },2000)
-
-  //   @HostListener('click')
-  //    ImageChange() {
-  //    this.prev.src = this.src;
-  //    for (let i = 0 ; i< this.imageSlide.length ; i++ ){
-  //     this.imageSlide[i].classList.remove('active');
-  //     console.log(this.imageSlide[1])
-  //    this.el.nativeElement.classList.add('active')}
 }
-
